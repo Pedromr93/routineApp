3c07beafa530982dc60846f72e23edd367e2f926
Pedro <pedro@Pedros-MacBook-Air.local>
2020-05-14 22:26:30 -0500

    feature: Added footer component, some Webpack settings and all static project resources

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import '../assets/styles/components/CarouselItem.scss'
+import '../assets/styles/components/CarouselItem.scss';
+import playIcon from '../assets/static/play-icon.png';
+import plusIcon from '../assets/static/plus-icon.png';
 
 const CarouselItem = () => (
     <div className="carousel-item">
         <img className="carousel-item__img" src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="code in a laptop"></img>
         <div className="carousel-item__details">
             <div>
-                <img className="carousel-item__details--img" src="./assets/icons/play-icon.png" alt="play button"></img>
-                <img className="carousel-item__details--img" src="./assets/icons/plus-icon.png" alt="plus button"></img>
+                <img className="carousel-item__details--img" src={playIcon} alt="play button"></img>
+                <img className="carousel-item__details--img" src={plusIcon} alt="plus button"></img>
             </div>
             <p className="carousel-item__details--title">Título descriptivo</p>
             <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import '../assets/styles/components/Header.scss'
+import '../assets/styles/components/Header.scss';
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import userIcon from '../assets/static/icons8-usuario-masculino-en-círculo-50.png';
 
 const Header = () => (
     <header className="header">
-        <img className="header__img" src="./logo-platzi-video-BW2.png" alt="nodejs"></img>
+        <img className="header__img" src={logo} alt="nodejs"></img>
         <div className="header__menu">
             <div className="header__menu--profile">
-                <img src="./icons8-usuario-masculino-en-círculo-50.png" alt="user"></img>
+                <img src={userIcon} alt="user"></img>
                 <p>Perfil</p>
             </div>
             <ul>
