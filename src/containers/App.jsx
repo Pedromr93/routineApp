419332a2eaed6ed4046b54b9227949d75c8f68c9
Pedro <pedro@Pedros-MacBook-Air.local>
2020-05-15 21:12:47 -0500

    feature: Added some setting for a fake api with json-server

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../assets/styles/app.scss'
 import Header from '../components/Header';
 import Search from '../components/Search';
@@ -7,45 +7,59 @@ import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 
-const App = () => (
-    <div className= "App">
-        <Header />
-        <Search />
-        <Categories title='Mi lista'>
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+const App = () => {
 
-        <Categories title='Tendencias' >
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    const [ videos, setVideos ] = useState([]);
+    const API = 'http://localhost:3000/initialState';
 
-        <Categories title='Originales de platzi video'>
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    useEffect( () => {
+        fetch(API)
+            .then(response => response.json())
+            .then(data => setVideos(data));  
+    }, []);
 
-        <Footer />
-    </div>
-);
+    console.log(videos)
+
+    return (
+        <div className= "App">
+            <Header />
+            <Search />
+            <Categories title='Mi lista'>
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Categories title='Tendencias' >
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Categories title='Originales de platzi video'>
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
+    )
+};
 
 export default App;
